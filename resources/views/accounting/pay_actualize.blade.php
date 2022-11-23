638dd9d4f8125e87b5a16a971c99cb0f8d4f1457
fgonzalez <felmiquiztli@gmail.com>
2022-11-23 13:25:36 -0600

    pay actualize front corrected

--- resources/views/accounting/pay_actualize.blade.php
+++ resources/views/accounting/pay_actualize.blade.php
@@ -23,31 +23,35 @@
                         <tr>
                             
                         </tr>
+                        
 </table>
+           <div class ="row">  
+            <div class ="col"></div>
+            <div class ="col">
 
-            <div class="col-sm-12 text-center font-bold text-sm">Actualizar  el pago
-
-            <table class="table table-striped">
+            <table class="table table-striped table-sm" style="width : 500px; align-self : center; display:flex">
                 <tbody>
                     <tr>
-                        <td style="background-color:#A6ADBC"> Cliente</td>
+                        <td style="background-color:#A6ADBC"> Cliente: </td>
                         <td>Cliente de Prueba 01</td>
                     </tr>
                     <tr>
-                        <td style="background-color:#A6ADBC"> Pedido </td>
+                        <td style="background-color:#A6ADBC"> Pedido: </td>
                         <td>{{$order->invoice}}</td>
                     </tr>
                     <tr>
-                        <td style="background-color:#A6ADBC"> Cantidad del pago</td>
+                        <td style="background-color:#A6ADBC"> Cantidad del pago: </td>
                         <td> $ {{number_format($pay -> amount)}}</td>
                     </tr>
                     <tr>
-                        <td>Concepto</td>
+                        <td  style="background-color:#A6ADBC">Concepto: </td>
                         <td>{{ $pay ->concept }}</td>
                     </tr>
                 </tbody>
             </table>
             </div>
+            <div class ="col"></div>
+           </div>
             <div>
             <form action="{{ route('accounting.pay_apply')}}" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -69,17 +73,17 @@
                 <p>Marcar como Pagado</p></button>
                 @else
 
-                <h1>Pago completado</h1>
+                <h1 style ="color : green; font-size: 30px" >Pago completado</h1>
               <br>
              <button type="button" onclick = "openPDF()"  class="btn btn-blue" > 
              <i class="fa-solid fa-eye fa-2x"></i> &nbsp; Ver comprobante  </button>
              
-             <br><br><br>
-
-             <p>Hubó algun problema con la validación del pago o fue revocadó?</p>
+             <br><br><br> <br>
+             <div class="col" style="background-color : #d9d9d9 ; width : 220px ; align-self: center; ">  
+             <p>¿Hubó algun problema con la validación  o el pago  fue revocadó?</p>
              <a href="{{route('pay_cancel',$pay->id)}}">
              <button type="button" ><span class="badge badge-pill badge-danger" style="font-size : 20px"> Invalidar pago</span></button>
-              </a>
+              </a></div>
                 @endif
                                 
                 </form>
