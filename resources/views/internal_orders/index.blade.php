acf6b0b0ee45c3a43beac6375a8496861b060322
fgonzalez <felmiquiztli@gmail.com>
2023-01-11 12:23:30 -0600

    ddd

--- resources/views/internal_orders/index.blade.php
+++ resources/views/internal_orders/index.blade.php
@@ -7,7 +7,7 @@
     <link href="{{ asset('vendor/fontawesome-free/css/fontawesome.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-      <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 @stop
 
