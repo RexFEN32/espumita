0b3513f1f0521788f4593b11a4770db016a39793
fgonzalez <felmiquiztli@gmail.com>
2023-02-14 14:25:37 -0600

    asda

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::group(['middleware' => ['auth']], function()
-{Route::post('internal_orders/capture', [InternalOrderController::class, 'capture'])->name('internal_orders.capture');
+{
     Route::resource('internal_orders', InternalOrderController::class);
     Route::resource('temp_items', TempItemController::class);
     Route::resource('items', ItemController::class);
@@ -43,7 +43,7 @@ Route::group(['middleware' => ['auth']], function()
     Route::get('tempitems/edit/{id}', [TempItemController::class, 'edit_item'])->name('tempitems.edit_item');
     Route::get('items/edit/{id}', [ItemController::class, 'edit_item'])->name('items.edit_item');
     Route::get('internal_orders/edit/{id}', [InternalOrderController::class, 'edit_order'])->name('internal_orders.edit_order');
-    
+    Route::post('internal_orders/capture', [InternalOrderController::class, 'capture'])->name('internal_orders.capture');
     Route::post('internal_orders/firmar', [InternalOrderController::class, 'firmar'])->name('internal_orders.firmar');
     Route::post('internal_orders/redefine', [InternalOrderController::class, 'redefine'])->name('internal_orders.redefine_order');
     Route::post('internal_orders/shipments', [InternalOrderController::class, 'shipment'])->name('internal_orders.shipment');
